62411472b22f2ad696003a7ab4505de0971714cb
Evgenii <sergeevjane@mail.ru>
2022-10-07 15:59:33 +0300

    added hw6

---
+++ qa_guru_15_6_pageobjects/src/test/java/com/demoqa/tests/RegistrationFormWithPageObjectsTests.java
@@ -0,0 +1,73 @@
+package com.demoqa.tests;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import com.demoqa.pages.RegistrationFormPage;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+
+public class RegistrationFormWithPageObjectsTests {
+    RegistrationFormPage registrationFormPage = new RegistrationFormPage();
+
+    @BeforeAll
+    static void configure() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.holdBrowserOpen = true;
+    }
+
+    @Test
+    void fillFormTest() {
+        registrationFormPage.openPage()
+                .setFirstName("Alex")
+                .setLastName("Egorov")
+                .setEmail("alex@egorov.com")
+                .setGender("Other")
+                .setNumber("1234567890")
+                .setBirthDate("30", "July", "2008")
+                .setSubjects("Maths")
+                .setHobbies("Sports")
+                .uploadPicture("img/Picture 1.png")
+                .setCurrentAddress("Some address 1")
+                .setState("NCR")
+                .setCity("Delhi")
+                .clickSubmitButton();
+
+        registrationFormPage.checkResultsTableVisible()
+                .checkResult("Student Name", "Alex Egorov")
+                .checkResult("Student Email", "alex@egorov.com")
+                .checkResult("Date of Birth", "30 July,2008")
+                .checkResult("Gender", "Other")
+                .checkResult("Mobile", "1234567890")
+                .checkResult("Subjects", "Maths")
+                .checkResult("Hobbies", "Sports")
+                .checkResult("Picture", "Picture 1.png")
+                .checkResult("Address", "Some address 1")
+                .checkResult("State and City", "NCR Delhi");
+    }
+
+    @Test
+    void fillFormWithMinimumDataTest() {
+        registrationFormPage.openPage()
+                .setFirstName("Alex")
+                .setLastName("Egorov")
+                .setGender("Other")
+                .setNumber("1234567890")
+                .setBirthDate("30", "July", "2008")
+                .clickSubmitButton();
+
+
+        registrationFormPage.checkResultsTableVisible()
+                .checkResult("Student Name", "Alex Egorov")
+                .checkResult("Date of Birth", "30 July,2008")
+                .checkResult("Gender", "Other")
+                .checkResult("Mobile", "1234567890");
+
+    }
+
+}
